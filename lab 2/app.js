66d86a0f3e2ac098431e6e55ead206ca983cbbb0
Elijah Prom <78039979+S2Mackinley@users.noreply.github.com>
2021-02-09 18:19:41 -0800

    created JavaScript for page

---
+++ lab 2/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+let myName = prompt('Is My Name Elijah?');
+
+    if (myName === 'yes') {
+        alert('correct');
+    }
+    else if (myName === 'no'){
+        alert('wrong')
+    }
+    else {
+        alert('please write yes or no')
+    }
+
+
+let myAge = prompt('Am I 25?');
+
+    if (myAge === 'yes') {
+        alert('correct');
+    }
+    else if (myAge === 'no'){
+        alert('wrong')
+    }
+    else {
+        alert('please write yes or no')
+    }
+
+
+
+let dogs = prompt('Do i have 4 dogs?');
+
+    if (dogs === 'yes') {
+        alert('wrong');
+    }
+    else if (dogs === 'no'){
+        alert('correct');
+    }
+    else {
+        alert('please write yes or no');
+    }
+
+let job = prompt('Was my previous job a hair stylist?');
+
+    if (job === 'yes') {
+        alert('wrong');
+    }
+    else if (job === 'no'){
+        alert('correct');
+    }
+    else {
+        alert('please write yes or no');
+    }
+
+let gf = prompt('Is my girlfriends name Hannah?');
+
+    if (gf === 'yes') {
+        alert('correct!');
+    }
+    else if (gf === 'no'){
+        alert('wrong')
+    }
+    else {
+        alert('please write yes or no');
+    }
+
+let userName = prompt('What is your name?');
+    alert('HI ' + userName + ' welcome to my site!' )
